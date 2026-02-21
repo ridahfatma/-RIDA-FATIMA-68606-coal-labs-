990399257117bdbe01b9ce815d32d188ac5602b8
ridahfatma <rida96816@gmail.com>
2026-02-22 04:37:21 +0500

    Add files via upload

---
+++ COAL LAB 4 TASK 02.asm
@@ -0,0 +1,20 @@
+.model small
+.stack 100h
+.data
+msg db " Rida Fatima -  68606"
+
+.code
+main proc
+
+mov ax, @data
+mov ds, ax
+
+mov dx, offset msg
+mov ah, 09h
+int 21h
+
+mov ah, 4ch
+int 21h
+
+main endp
+end main
